738375280d41435eea69980c53964f75a2ff246c
Matt <matt.e.greer@gmail.com>
2011-10-10 07:37:03 -0600

    Initial start of exploring rectangle and grid

---
+++ scratch/Rectangle.js
@@ -0,0 +1,15 @@
+
+function Rectangle(x, y, width, height) {
+
+};
+
+Rectangle.prototype = {
+	get x(),
+	get y(),
+	get width(),
+	get height(),
+	equals: function(otherRect) {
+	},
+	clone()
+}
+
