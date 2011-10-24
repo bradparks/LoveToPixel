9b4679707be2166f8905a6bc633d5df925090aa7
Matt Greer <matt.e.greer@gmail.com>
2011-10-23 19:55:54 -0700

    Added readme link to statusbar

--- app/js/ui/StatusBar.js
+++ app/js/ui/StatusBar.js
@@ -22,7 +22,8 @@
 				xtype: 'container',
 				html: '<b>LoveToPixel: extremely early code, not at all done or ready, stay tuned! </b>' + 
 					'<a href="mailto:matt.e.greer@gmail.com?subject=LTP">matt.e.greer@gmail.com</a>, ' +
-					'<a href="https://github.com/city41/LoveToPixel" target="_blank">github</a> '
+					'<a href="https://github.com/city41/LoveToPixel" target="_blank">github</a>, ' +
+					'<a href="https://github.com/city41/LoveToPixel/blob/master/README.md" target="_blank">README</a>'
 			}
 		]
 	});
