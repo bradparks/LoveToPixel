61bd034130fe949b02f035a0142dd5b88bf8a3c3
Matt Greer <matt.e.greer@gmail.com>
2011-11-13 01:40:40 -0700

    Tiny typo fix

--- app/js/ui/StatusBar.js
+++ app/js/ui/StatusBar.js
@@ -26,7 +26,7 @@
 			},
 			{
 				xtype: 'container',
-				html: '<b>LoveToPixel: alpha</b>' + 
+				html: '<b>LoveToPixel: alpha </b>' + 
 					'<a href="mailto:matt.e.greer@gmail.com?subject=LTP">matt.e.greer@gmail.com</a>, ' +
 					'<a href="https://github.com/city41/LoveToPixel" target="_blank">github</a>, ' +
 					'<a href="https://github.com/city41/LoveToPixel/blob/master/README.md" target="_blank">README</a>'
